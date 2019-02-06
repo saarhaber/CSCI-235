79ea2bffaf6e33d4c7389d98d3e1b9c1945d31ed
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-02-06 16:34:09 -0500

    initial upload

---
+++ Project 1/CourseMember.cpp
@@ -0,0 +1,35 @@
+/**
+Name: Saar Haber
+Course: CS 235
+Assingment: Project 1A
+An abstraction for a person in this course.
+A CourseMember will always have a first name, last name and ID.
+All we can do with a CourseMember, for now, is retrieve that information.
+Date: 1/29/19
+*/
+
+#include <string>
+#include "CourseMember.hpp"
+using namespace std;
+
+/** constructor */
+CourseMember::CourseMember(int id, string first, string last) {
+  id_ = id;
+  first_name_ = first;
+  last_name_ = last;
+}
+
+/** @return returns id_; */
+int CourseMember::getID() const {
+  return id_;
+}
+
+/** @return returns first_name_ */
+string CourseMember::getFirstName() const {
+  return first_name_;
+}
+
+/** @return returns last_name_ */
+string CourseMember::getLastName() const {
+  return last_name_;
+}
---
+++ Project 1/main.cpp
@@ -0,0 +1,9 @@
+int main () {
+
+//testing
+CourseMember Saar = {23, "saar", "haber"};
+cout << Saar.getID() << endl;
+cout << Saar.getFirstName() << endl;
+cout << Saar.getLastName() << endl;
+
+}
